09cc6ff2eac7c1ae6ec0772a3f013e29999c23d4
kozar1994 <vovakozar@gmail.com>
2025-08-26 12:23:27 +0300

    feat: Add initial modules for app logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './libs/prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
+import { PlanModule } from './modules/plan/plan.module';
+import { InvoiceService } from './modules/invoice/invoice.service';
+import { InvoiceModule } from './modules/invoice/invoice.module';
+import { SubscriptionModule } from './modules/subscription/subscription.module';
+import { PaymentModule } from './modules/payment/payment.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule, PlanModule, InvoiceModule, SubscriptionModule, PaymentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, InvoiceService],
 })
 export class AppModule {}
